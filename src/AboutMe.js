f79c45973b8f953c94ff960ba51c49a19f0ce382
Ahmed <ahmedskulj10@gmail.com>
2021-05-01 19:42:29 +0200

    AboutMe and Navigation(title) added.

---
+++ src/AboutMe.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./AboutMe.css";
+function AboutMe() {
+  return (
+    <div className="about-me">
+      <div className="profile-photo"></div>
+      <div className="aboutme-section">
+        <h1>Who am I?</h1>
+        <p>
+          Hi, my name is Ahmed Škulj. I am from Bosnia and Herzegovina.
+          Currently I am studying Software Engineering at the University of
+          Zenica. I started coding about two years ago, and it since then I made
+          over 20 different projects, big and small. I prefer building frontend
+          minimalistic apps, but I have also worked on some backend projects.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default AboutMe;
---
+++ src/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navigation.css";
+function Navigation() {
+  return (
+    <div>
+      <div className="title">
+        <h3>Ahmed Škulj™</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
